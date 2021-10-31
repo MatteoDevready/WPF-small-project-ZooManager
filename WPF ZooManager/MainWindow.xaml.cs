8c774cd70578ed67f3a1490ca664c439d44a8ab8
Matteo Cannas <matteocannas1990@outlook.it>
2021-10-31 12:54:23 +0100

    Showing Data from DB in the ListBox

--- WPF ZooManager/MainWindow.xaml.cs
+++ WPF ZooManager/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +23,45 @@ namespace WPF_ZooManager
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        SqlConnection sqlConnection;
+
         public MainWindow()
         {
             InitializeComponent();
 
             string connectionString = ConfigurationManager.ConnectionStrings["WPF_ZooManager.Properties.Settings.MatteoRealDBConnectionString"].ConnectionString;
+
+            sqlConnection = new SqlConnection(connectionString);
+            ShowZoos();
+        }
+
+        private void ShowZoos()
+        {
+            try
+            {
+                string query = "select * from Zoo";
+                //the SQL Data adapter can be imagined like an interface to make tables Usable by C# Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+                    sqlDataAdapter.Fill(zooTable);
+
+                    //Which information of the table in datatable should be shown in the listbox?
+                    listZoos.DisplayMemberPath = "Location";
+                    //which values should be delivered, when an item from the list box is selected?
+                    listZoos.SelectedValuePath = "Id";
+                    //The reference to the data the listbox should populate
+                    listZoos.ItemsSource = zooTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
         }
     }
 }
